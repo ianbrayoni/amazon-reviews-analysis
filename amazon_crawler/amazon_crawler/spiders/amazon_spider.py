8dee8c9e14ecaae126b7486cf46d75622dfeac3e
Ian Brayoni <ibaraza.ib@gmail.com>
2016-08-09 05:14:51 +0300

    Fn to loop through list of pages

--- amazon_crawler/amazon_crawler/spiders/amazon_spider.py
+++ amazon_crawler/amazon_crawler/spiders/amazon_spider.py
@@ -3,7 +3,7 @@
 """
  https://www.amazon.com/Samsung-UN40H5003-40-Inch-Measured-Diagonally/
  product-reviews/B00MYBR75O/pageNumber=2
- 
+
 """
 
 import scrapy
@@ -17,11 +17,22 @@ class AmazonSpiderSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        pass
+        # get the last page number on the page
+        last_page_num = self.last_page_numerical(response)
 
+        if last_page_num < 1:
+            # abort the search if there are no results
+            return
+        else:
+            # otherwise loop over all pages and scrape
+            page_urls = [response.url + "pageNumber=" + str(pageNumber)
+            for pageNumber in xrange(1,last_page_num + 1)]
 
-    def last_page_num(self, response):
-    	#
+            for page_url in page_urls:
+                yield scrapy.Request(page_url, callback=self.parse_reviews)
+
+    def last_page_numerical(self, response):
+    	# retrieve last page number from the pagination
     	try:
     	 	last_page_num = int(response
     	 		.xpath('//ul[@class="a-pagination"]/li[last()-1]/a/@href')
@@ -30,3 +41,6 @@ class AmazonSpiderSpider(scrapy.Spider):
     	 	return last_page_num
     	 except Exception, e:
     	 	raise e 
+
+    def parse_reviews(self, response):
+        pass
