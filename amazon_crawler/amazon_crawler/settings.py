1b1a62a76bb8a3dd1b3884631a4859a93ea922b8
Ian Brayoni <ibaraza.ib@gmail.com>
2016-08-14 13:02:07 +0300

    Created User Agent to emulate a browser.

--- amazon_crawler/amazon_crawler/settings.py
+++ amazon_crawler/amazon_crawler/settings.py
@@ -17,6 +17,7 @@ NEWSPIDER_MODULE = 'amazon_crawler.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'amazon_crawler (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
